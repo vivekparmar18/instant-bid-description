0cf1953a965de130d2d2f471448da7be1952a9ce
vivek.parmar <vivek.parmar@marutitech.com>
2024-08-12 10:43:49 +0530

    ui changes

--- run.py
+++ run.py
@@ -55,63 +55,37 @@ def display_descriptions(formatted_data, paragraph_description):
 
     with col1:
         with st.container(height=600):
-            st.subheader('Instant Bid API Response', divider='red')
+            st.subheader('Instant Bid API JSON Response', divider='red')
             st.code(formatted_data)
     with col2:
         with st.container(height=600):
-            st.subheader('Paragraph Description', divider='red')
+            st.subheader('Description', divider='red')
             st.write(paragraph_description)
-
-
-def format_json(data, indent=0):
-    formatted_str = ""
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, dict):
-                formatted_str += " " * indent + f"{key}:\n"
-                formatted_str += format_json(value, indent + 4)
-            elif isinstance(value, list):
-                formatted_str += " " * indent + f"{key}:\n"
-                for item in value:
-                    formatted_str += format_json(item, indent + 4)
-            else:
-                formatted_str += " " * indent + f"{key}: {value}\n"
-    elif isinstance(data, list):
-        for item in data:
-            formatted_str += format_json(item, indent)
-    else:
-        formatted_str += " " * indent + f"- {data}\n"
-    return formatted_str
 
 
 async def main():
     st.set_page_config(
-        page_title="Instant Bid Description",
+        page_title="Instant Bid - Description Generator PoC",
         page_icon=":car:",
         layout="wide"
     )
 
-    st.title("Instant Bid Description")
+    st.title("Instant Bid - Description Generator PoC")
     st.divider()
-    st.subheader("Generate description from Instant Bids with ease !🚗")
+    st.subheader("Provide your Instant Bid ID to generate description\n "
+                 "(Sample IDs: 682095, 682096, 682097, 682101, 682102)")
 
-    json_data = display_sample_jsons()
-    st.subheader("OR")
-    data = get_json_file()
-
-    char_limit = get_char_limit()
+    data = """"""
 
     description_generator = DescriptionGenerator()
 
-    if st.button("Generate Description"):
-        with st.spinner("Generating Description..."):
-            if data:
-                formatted_data = format_json(data)
-                paragraph_description = await description_generator.get_paragraph_description(data, char_limit)
-            else:
-                formatted_data = format_json(json_data)
-                paragraph_description = await description_generator.get_paragraph_description(json_data, char_limit)
-        display_descriptions(formatted_data, paragraph_description)
+    instant_bid_id = st.text_input("Instant Bid ID", label_visibility="collapsed")
+
+    if st.button("Generate"):
+        print("data")
+        # with st.spinner("Generating Description..."):
+        #     paragraph_description = await description_generator.get_paragraph_description("json_data", "char_limit")
+        # display_descriptions(data, paragraph_description)
 
 
 if __name__ == "__main__":
